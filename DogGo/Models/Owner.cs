24cc6de2b4f61daa59e86b6bcfae658b742ff9e9
r-stroud <stroudrobert86@gmail.com>
2023-04-03 21:44:05 -0500

    Add project files.

---
+++ DogGo/Models/Owner.cs
@@ -0,0 +1,13 @@
+﻿namespace DogGo.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int NieghborhoodId { get; set; }
+        public Neighborhood neighborhood { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ DogGo/Repositories/IOwnerRepository.cs
@@ -0,0 +1,14 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+
+namespace DogGo.Repositories
+{
+    public interface IOwnerRepository
+    {
+        SqlConnection Connection { get; }
+
+        List<Owner> GetAllOwners();
+
+        Owner GetOwnerById(int id);
+    }
+}
